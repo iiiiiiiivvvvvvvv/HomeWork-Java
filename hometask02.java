08de95f22015e8ae4842a2337861f51425bde1bb
Igor <oligor2@yandex.ru>
2023-02-12 11:43:15 +0500

    02

---
+++ hometask02.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// Вывести все простые числа от 1 до 1000
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class hometask02 {
+    public static void  main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите положительное число: ");
+        int input = scanner.nextInt();
+        scanner.close();
+        List <Integer> Primes = new ArrayList<>();
+
+        for(int i = 2; i <= input; i++) {
+            boolean isPrimeNumber = true;
+
+            for (int j = 2; j < i; j++) {
+                if(i % j == 0) {
+                    isPrimeNumber = false;
+                    break;
+                }
+            }
+            if(isPrimeNumber) {
+                Primes.add(i);
+            }
+        }
+        System.out.println("Простые числа: " + Primes);
+    }
+    
+}
